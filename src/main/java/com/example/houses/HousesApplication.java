9d3d2ca7d64d90dd18e0db221898d6e7f66c0a08
ivana-queiroz-assis <ivanaqueirozassis@hotmail.com>
2019-07-20 22:01:44 -0300

    Commit with GCP Storage.

--- src/main/java/com/example/houses/HousesApplication.java
+++ src/main/java/com/example/houses/HousesApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.util.StreamUtils;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.OutputStream;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -21,9 +26,18 @@ public class HousesApplication {
 
 	@Bean
 	public CommandLineRunner houses(HouseRespository houseRespository,
-									PubSubTemplate pubSubTemplate) {
+									PubSubTemplate pubSubTemplate, ApplicationContext context) {
 		return args -> {
-			Stream.of(new House("Rua Tijuca 160 Leblon"),
+
+			Resource houseIvana = context.getResource("https://img.olx.com.br/images/43/433915011671006.jpg");
+			Resource gcsPicture = context.getResource("gs://springone-houses/houseIvana.jpg");
+
+			byte[] pictureBytes = StreamUtils.copyToByteArray(houseIvana.getInputStream());
+
+			try(OutputStream os = ((WritableResource) gcsPicture).getOutputStream()){
+				os.write(pictureBytes);
+			}
+			Stream.of(new House("Rua Tijuca 160 Leblon", gcsPicture.getURI().toString()),
 					new House("Rua Professor Aldo Zanini 873 Vila Operária"),
 					new House("Avenida Alameda Oscar Niermayer 119 Nova Lima"),
 					new House("North Pole"))
@@ -45,6 +59,19 @@ class House{
 	@GeneratedValue
 	private Long id;
 	private String address;
+	private String gcsUri;
+
+	public House(String address, String gcsUri) {
+
+		this.address = address;
+		this.gcsUri = gcsUri;
+	}
+	public House() {
+	}
+
+	public House( String address) {
+		this.address = address;
+	}
 
 	public Long getId() {
 		return id;
@@ -59,15 +86,15 @@ class House{
 	}
 
 	public String getAddress() {
-
 		return address;
 	}
 
-	public House() {
+	public String getGcsUri() {
+		return gcsUri;
 	}
 
-	public House( String address) {
-		this.address = address;
+	public void setGcsUri(String gcsUri) {
+		this.gcsUri = gcsUri;
 	}
 }
 
